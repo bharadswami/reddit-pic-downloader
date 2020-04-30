04c67af3eb3f7444b611495ed0ebd67e64e84d28
Bharadwaj Swaminathan <bharad.swami@gmail.com>
2020-04-30 02:20:29 -0700

    Changed naming of images to prevent overwriting

--- wallpaper.py
+++ wallpaper.py
@@ -16,6 +16,7 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
                      client_secret=SECRET,
                      user_agent='wallpaper downloader')
 
+
 def good_pic(submission):
     """
     Returns True if the picture is larger than the minimum width and height
@@ -28,6 +29,7 @@ def good_pic(submission):
     source = submission.preview['images'][0]['source']
     pic_width, pic_height = source['width'], source['height']
     return pic_width >= MIN_WIDTH and pic_height >= MIN_HEIGHT and pic_width > pic_height
+
 
 def get_top_pics(sub):
     """
@@ -47,15 +49,17 @@ def get_top_pics(sub):
         if num_downloaded >= pics_per_day:
             break
 
+
 def download_image(subreddit, url):
     """
     Downloads an image from the given URL
     """
     resp = requests.get(url, stream=True)
-    filename = DEST_DIR + subreddit + datetime.now().strftime("%Y%m%dT%H%M%S") + ".jpg"
+    filename = DEST_DIR + subreddit + datetime.now().strftime("%Y%m%dT%H%M%S.%f") + ".jpg"
     with open(filename, 'wb+') as file:
         resp.raw.decode_content = True
         shutil.copyfileobj(resp.raw, file)
+
 
 def cleanup(num_days, dest_dir):
     """
@@ -64,23 +68,23 @@ def cleanup(num_days, dest_dir):
     threshold = time.time() - (num_days * 24 * 60 * 60)
     for _, _, files in os.walk(dest_dir, topdown=False):  # os.walk returns root, directories, files
         for file in files:
-            if file != ".DS_Store":  # To prevent cluttering trash with all the .DS_Store files
-                full_path = os.path.join(dest_dir, file)
-                file_age = os.stat(full_path).st_mtime
+            full_path = os.path.join(dest_dir, file)
+            file_age = os.stat(full_path).st_mtime
 
-                if file_age < threshold:
-                    try:
-                        shutil.move(full_path, TRASH_PATH)
-                        logging.info(f"Moved {file} to trash")
-                    except Exception as e:
-                        logging.info(f"Unable to delete {file}")
+            if file_age < threshold:
+                try:
+                    shutil.move(full_path, TRASH_PATH)
+                    logging.info(f"Moved {file} to trash")
+                except Exception as e:
+                    logging.info(f"Unable to delete {file}\n {e}")
 
-logging.info("Launched script")
 
 if __name__ == "__main__":
     try:
+        logging.info("Launched script")
         get_top_pics(SUBREDDIT)
         cleanup(CLEANUP_DAYS, DEST_DIR)
     except NewConnectionError:
         logging.info("Unable to establish connection - couldn't download image")
-
+    except Exception as e:
+        logging.info(e)
